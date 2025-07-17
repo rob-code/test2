f3ede40b5698046d2aee272d272599026c56a76d
robertbrice <rob.a.brice@gmail.com>
2025-07-17 16:59:13 +0100

    updates

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+let {children} = $props();
+</script>
+
+<nav>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+</nav>
+
+{@render children()}
+
+
+
