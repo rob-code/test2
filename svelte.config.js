30c5ba67448390e35ddc1f39f69efcae13c148b4
robertbrice <rob.a.brice@gmail.com>
2025-07-21 17:49:52 +0100

    update config

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
 //import adapter from '@sveltejs/adapter-auto';
 //import adapter from '@sveltejs/adapter-static';
 import adapter from 'amplify-adapter';
-
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -15,15 +14,15 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: true,
-		}),
-		paths: {			
-			base:process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+			// pages: 'build',
+			// assets: 'build',
+			// fallback: 'index.html',
+			// precompress: false,
+			// strict: true,
+		})
+		// paths: {			
+		// 	base:process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		// }
 	}
 };
 
