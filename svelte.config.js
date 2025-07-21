9211d1554492b86a0c18668fad5da730c8717ea7
robertbrice <rob.a.brice@gmail.com>
2025-07-21 18:54:00 +0100

    more stuff

--- svelte.config.js
+++ svelte.config.js
@@ -14,9 +14,9 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
+			// pages: 'build',
+			// assets: 'build',
+			// fallback: 'index.html',
 			// precompress: false,
 			// strict: true,
 		 })
