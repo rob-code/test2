98571761449dc34d183dc515dcfa209ce6968caa
robertbrice <rob.a.brice@gmail.com>
2025-07-28 15:12:15 +0100

    updated nav links in project pages

--- src/routes/projects/true_love_hearts/+page.svelte
+++ src/routes/projects/true_love_hearts/+page.svelte
@@ -29,12 +29,12 @@
       <div class="container">
       <div class="row project-pages-next-page">
         <div class="col">
-          <p class="text-start" style="padding-left:4%" ><a href="colour-fields.html" class="link-black"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 20 20">
+          <p class="text-start" style="padding-left:4%" ><a href="/projects/colour_fields" class="link-black"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 20 20">
             <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>
           </svg> Colour Fields</a></p>
         </div>
         <div class="col">
-          <p class="text-end" style="padding-right:4%"><a href="../index.html" class="link-black">Projects <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 20 20">
+          <p class="text-end" style="padding-right:4%"><a href="../projects" class="link-black">Projects <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 20 20">
             <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/>
           </svg></a></p>  
         </div>
