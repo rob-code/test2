684ede14b5cff7adb0b002c93b3275726f08fb70
robertbrice <rob.a.brice@gmail.com>
2025-07-30 11:49:42 +0100

    better layout for everyone can play

--- src/routes/projects/everyone_can_play/+page.svelte
+++ src/routes/projects/everyone_can_play/+page.svelte
@@ -8,14 +8,8 @@
 
    <section class="container">
         <h2 class="fw-light text-center project-page-header">Everyone Can Play</h2>
-        <p class="lead text-body-secondary project-description-left"> In the middle of 2023 the Independent Commission for Equity in Cricket (ICEC) released a report "Holding up a Mirror to Cricket". A subsidiary report "It's Not Banter: Lived experiences of discrimination in cricket" prepared by The Foundation for the ICEC provided additional and powerful testimony of the effect of discrimination on peoples' lives and wellbeing. It's a disturbing read. Discrimination is also present in rugby and football. But of course it is not just sport, taken at its fullest, the ICEC report holds up a mirror to UK society.</p>
-
-        <p class="lead text-body-secondary project-description-left">These reports struck a chord and almost immediately the idea for this series of images popped into my head. Using colour as a metaphor for diversity, "Everyone Can Play" is my response to these reports and the enduring issue of discrimination.</p>
-
-        <p class="lead text-body-secondary project-description-left item-padding-bottom">Sport in all its forms is better with wide participation. Positive attitudes to improvement and excellence, irrespective of background, make healthier and happier people who want to be active and involved. In other words, everyone can play.</p>
-
+        <p class="lead text-body-secondary project-description-centre item-padding-bottom">Sport in all its forms is better with wide participation. Positive attitudes to improvement and excellence, irrespective of background, make healthier and happier people who want to be active and involved. In other words, everyone can play.</p>
     </section>
-
 
     <div class="bg-body-tertiary">
       <div class="col mx-auto item-padding-top container">
@@ -41,7 +35,11 @@
       </div>     
     </div>
 
+   <section class="container">
+        <p class="lead text-body-secondary project-description item-padding-top"> In the middle of 2023 the Independent Commission for Equity in Cricket (ICEC) released a report "Holding up a Mirror to Cricket". A subsidiary report "It's Not Banter: Lived experiences of discrimination in cricket" prepared by The Foundation for the ICEC provided additional and powerful testimony of the effect of discrimination on peoples' lives and wellbeing. It's a disturbing read. Discrimination is also present in rugby and football. But of course it is not just sport, taken at its fullest, the ICEC report holds up a mirror to UK society.</p>
 
+        <p class="lead text-body-secondary project-description">These reports struck a chord and almost immediately the idea for this series of images popped into my head. Using colour as a metaphor for diversity, "Everyone Can Play" is my response to these reports and the enduring issue of discrimination.</p>
+    </section>
 
       <div class="container">
       <div class="row project-pages-next-page">
