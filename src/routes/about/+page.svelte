889071b857bba4396c01e12f7edd140963092e4b
robertbrice <rob.a.brice@gmail.com>
2025-08-04 14:35:04 +0100

    much improved about text

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -9,11 +9,10 @@
   <title>Robert Brice - About</title>
 </svelte:head>
 
-<PageTitle pagetitle='Robert Brice'/>
+<PageTitle pagetitle="Robert Brice" />
 
 <div class="container">
   <div class="row" style="padding-top: 2em;">
-
     <div class="col-md-5">
       <div class="card">
         <img
@@ -21,49 +20,39 @@
           alt="Portrait with large robot"
           class="card-img-top"
         />
-        <img
+        <!-- <img
           src={mewithcomputer1983}
           alt="Portrait with computer in 1983"
           class="card-img-top"
-        />
+        /> -->
       </div>
     </div>
     <div class="col-md-7">
       <p class="lead text-body-secondary">
-        The use of computers in static and moving image making is
-        incredibly broad and ever changing, new tools appear all the time. Its
-        easy to get diverted by the technology and to become overwhelmed by the
-        endless amount of new frameworks, languages and technqiues to learn.So
-        while I balance the need to learn and find the tools that fit with what
-        I want to achieve, I am drawn back always to a few simple priciples.
-        What do I want to say? What is the narrative, or increasingly I realise
-        what is the poem I am trying to write in the form of an image? Isn't an
-        image after all a piece of poetry, a story, a small nugget of meaning
-        portrayed in visual form? My paellete is the coide and my pigment are
-        the pixels that make up the image, but as always the challenge is the
-        idea and its execution.
+        As a digital artist I balance technique - the work to find and master
+        tools and processes that fit with what I want to achieve - with a few
+        simple priciples; what do I want to say, what is the story I want to
+        tell.</p> 
+        
+        <p class="lead text-body-secondary">
+        Increasingly I see the ideas that come to me as visual poems. After all, isn't an image a a story, a small fragment of
+        meaning portrayed in visual form? While my palette is code and my
+        pigments are pixels, as always, the greater challenge is the idea and
+        its execution in the form of an image which is both eloquent and
+        enjoyable.
       </p>
 
       <p class="lead text-body-secondary">
-        For the last 2 years, I have been developing a practice in digital art.
-        While the landscape of tools and technologies available to the digital
-        artist has never been greater and contiunues to grow, I have focused on
-        learning by creating and developing projects. Some of these projects are
-        shown here on my site. As time goes on and my range increases, I expect
-        what I make will change.
+        It is clear that there is a visual language which emerges in digital art
+        which is unique and distinctive. My goal is to keep developing my craft through explorations in this space. But to be clear, code is there to
+        serve, not dictate. To be manipulated. I do not make images using AI. If I have any sort of manifesto,
+        it is that my work will be built with human and not artificial
+        intelligence.
       </p>
       <p class="lead text-body-secondary">
-        So far I have developed projects in javascript, java, C++ and Blender. I
-        am currently learning vvvv.
-      </p>
-      <p class="lead text-body-secondary">
-        I have degrees in physics, environmental management and education, and
-        have enjoyed careers as a renewable energy technologist and as a physics
-        teacher. Alongside these day jobs I have always pursued interests in
-        music, art and sailing. The door into digital art opened when I realised
-        I could combine my interests in art and technology in one practice.
+        My work as a digital artist started in 2022 when I discovered I could
+        pursue my interests in art and technology in one unified practice.
       </p>
     </div>
-
   </div>
 </div>
