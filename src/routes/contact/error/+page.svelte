3336359cfcfc12450574083d7ddb3da87b628c78
robertbrice <rob.a.brice@gmail.com>
2025-08-04 12:57:02 +0100

    a bit of clean up

--- src/routes/contact/error/+page.svelte
+++ src/routes/contact/error/+page.svelte
@@ -13,6 +13,5 @@
 <main>
       <div class="container">
         <div class="fw-light title-size-reduced" style="padding-top: 2em;">There has been an error - please try again</div>	
-
       </div>
-    </main>
+</main>
--- src/routes/contact/success/+page.svelte
+++ src/routes/contact/success/+page.svelte
@@ -7,12 +7,9 @@
     <title>Robert Brice - Email Sent</title>
 </svelte:head>
 
-
-
-
 <main>
       <div class="container">
         <div class="fw-light title-size-reduced" style="padding-top: 2em;">Thank you.</div>	
         <div class="fw-light title-size-reduced" style="padding-top: 1em; padding-bottom: 3em;">Your message has been sent.</div>
       </div>
-    </main>
+</main>
