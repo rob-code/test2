73a6ae5149d8c990b9bd820fbd94f83935b9614d
robertbrice <rob.a.brice@gmail.com>
2025-08-21 13:23:02 +0100

    reformat

--- src/routes/shop/[id]/+page.svelte
+++ src/routes/shop/[id]/+page.svelte
@@ -1,33 +1,30 @@
-<script lang="ts">
+<script lang=ts>
     import PageTitle from "../../../components/pagetitle.svelte";
     import products from "$lib/data/products.json";
-    import { base } from "$app/paths";
+    import {base} from '$app/paths';
 
-    let { data } = $props();
-    console.log("taken from props: " + data.id);
+    let {data} = $props();
+    console.log("taken from props: " + data.id);    
 
-    let product = $state({
-        id: 0,
-        img: "",
-        alt: "",
-        name: "",
-        dataitemid: "",
-        price: "",
-        description: "",
-    });
+   
+   let product = $state({"id": 0, "img":"", "alt":"", "name":"", "dataitemid":"", "price":"", "description":"" });
 
-    $effect(() => {
-        for (var i = 0; i < products.length; i++) {
-            if (products[i].id == Number(data.id)) {
-                product = products[i];
-            }
+
+    $effect (() => {   
+    for (var i = 0; i < products.length; i++) {
+        if (products[i].id == Number(data.id)) {
+            product = products[i];
         }
-    });
+    }
+    }
+)
+
+
 </script>
 
 <svelte:head>
-    <!-- all the other meta tags go in there for the page -->
-    <title>Robert Brice - Add to Cart</title>
+  <!-- all the other meta tags go in there for the page -->
+  <title>Robert Brice - Add to Cart</title>
 </svelte:head>
 
 <div class="container py-5">
@@ -35,41 +32,20 @@
         <!-- Product Images -->
         <div class="col-md-6 mb-4">
             <div class="card">
-                <img
-                    src="{product?.img}"
-                    class="card-img-top"
-                    alt="Product"
-                />
+                <img src="{base}/{product?.img}" class="card-img-top" alt="Product">
                 <div class="card-body">
                     <div class="row g-2">
                         <div class="col-3">
-                            <img
-                                src="{product?.img}"
-                                class="img-thumbnail"
-                                alt="Thumbnail 1"
-                            />
+                            <img src="{base}/{product?.img}" class="img-thumbnail" alt="Thumbnail 1">
                         </div>
                         <div class="col-3">
-                            <img
-                                src="{product?.img}"
-                                class="img-thumbnail"
-                                alt="Thumbnail 2"
-                            />
+                            <img src="{base}/{product?.img}" class="img-thumbnail" alt="Thumbnail 2">
                         </div>
                         <div class="col-3">
-                            <img
-                                src="{product?.img}"
-                                class="img-thumbnail"
-                                alt="Thumbnail 3"
-                            />
+                            <img src="{base}/{product?.img}" class="img-thumbnail" alt="Thumbnail 3">
                         </div>
                         <div class="col-3">
-                            <img
-                                src="{product?.img}"
-                                class="img-thumbnail"
-                                alt="Thumbnail 4"
-                            />
-                        </div>
+                            <img src="{base}/{product?.img}" class="img-thumbnail" alt="Thumbnail 4">                        </div>
                     </div>
                 </div>
             </div>
@@ -83,7 +59,7 @@
                 <!-- <span class="text-muted text-decoration-line-through">$399.99</span>
                 <span class="badge bg-danger ms-2">25% OFF</span> -->
             </div>
-            <!-- 
+<!-- 
             <div class="mb-3">
                 <div class="d-flex align-items-center">
                     <div class="text-warning me-2">
@@ -128,9 +104,7 @@
 
             <!-- Actions -->
             <div class="d-grid gap-2">
-                <button class="btn btn-primary" type="button"
-                    >Add to Cart</button
-                >
+                <button class="btn btn-primary" type="button">Add to Cart</button>
             </div>
 
             <!-- Additional Info -->
@@ -151,3 +125,4 @@
         </div>
     </div>
 </div>
+
