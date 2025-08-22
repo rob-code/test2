11b093b1f9ef6086871194e80abc10dd4bf5641f
robertbrice <rob.a.brice@gmail.com>
2025-08-22 10:23:42 +0100

    included product image in link

--- src/components/salegrid.svelte
+++ src/components/salegrid.svelte
@@ -7,8 +7,13 @@
         <div class="row row-cols-1 row-cols-sm-3 row-cols-md-3 g-3">
             {#each prints as print}
                 <div class="col">
-                    <img src={print.img1} alt={print.alt} class="card-img-top" />
-                    <div><a href='shop/{print.id}' class="print-title">{print.name}</a></div>
+                    <a href="shop/{print.id}" class="print-title">
+                        <img
+                            src={print.img1}
+                            alt={print.alt}
+                            class="card-img-top"
+                        />{print.name}</a
+                    >
                     <div class="price">£ {print.price}</div>
                 </div>
             {/each}
