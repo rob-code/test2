e7619bf8a343a288953180a7e175e711b6d6c387
robertbrice <rob.a.brice@gmail.com>
2025-09-04 09:33:38 +0100

    removed snipcart navigation cart button

--- src/components/navigation.svelte
+++ src/components/navigation.svelte
@@ -59,16 +59,15 @@
         </li>
       </ul>
     </div>
-    <button class="snipcart-checkout cart-button" aria-label="blah blah"
+    <!-- <button class="snipcart-checkout cart-button" aria-label="blah blah"
       ><svg class="cart-icon">
         <path
           d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"
         />
       </svg>
       <span class="position-absolute badge rounded-pill bg-dark"
-        ><span class="snipcart-items-count"></span></span
-      >
-    </button>
+        ><span class="snipcart-items-count"></span></span>
+    </button> -->
   </div>
 </nav>
 
