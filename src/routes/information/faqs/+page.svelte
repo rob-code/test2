0be76b4fa1dacdd3d583be3da0aefdae3f1b2743
robertbrice <rob.a.brice@gmail.com>
2025-09-22 18:25:22 +0100

    first attempt at FAQs

--- src/routes/information/faqs/+page.svelte
+++ src/routes/information/faqs/+page.svelte
@@ -1,1 +1,51 @@
-FAQs
+
+
+<div class="container">
+
+<div class="display-6 page-title">FAQs</div>
+
+
+<div class="faq-item">
+<span>What are Giclee prints?
+  <a data-bs-toggle="collapse" href="#gicleePrints" role="button" aria-expanded="false" aria-label="collapseExample">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="plus-symbol"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+  </a></span>
+</div>
+
+
+<div class="collapse" id="gicleePrints">
+  <!-- <div class="card card-body"> -->
+Giclee prints will last up to 75 years and are the best the art world can offer. They are produced using 12 pigment inks which reproduce around 98% of the colour spectrum. They will resist fading even when exposed to sunlight. The paper is archival quality ph neutral 280gsm Natural White Smooth Cotton. Note that the colour of the print may vary slightly from that shown on your monitor. The prints feel wonderful and look great.
+  <!-- </div> -->
+</div>
+
+
+<div class="faq-item">
+<span>Are the images made using AI?
+  <a data-bs-toggle="collapse" href="#noAi" role="button" aria-expanded="false" aria-label="collapseExample">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="plus-symbol"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+  </a></span>
+</div>
+
+<div class="collapse" id="noAi">
+
+I have written the computer code that has generated this image. It is not drawn or photographed. AI has not been used in any part of the process. The combinations of colour, shape and so on are generated randomly so I often have to produce many print options before I find one that feels like it sings. The print you see here is one of my favourites from that process. Every print is signed.
+
+</div>
+
+<div class="faq-item">
+<span>What are the sizes ofthe images like on my wall?
+  <a data-bs-toggle="collapse" href="#printDimensions" role="button" aria-expanded="false" aria-label="collapseExample">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="plus-symbol"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+  </a></span>
+</div>
+
+<div class="collapse" id="printDimensions">
+
+The print dimensions refer to the size of the print, not the paper. To allow mounting and framing, every print has an extra unprinted border top, left and right of 2 cm, and a larger bottom border of 4cm to provide additional space for the artists signature.
+
+</div>
+
+
+
+</div>
