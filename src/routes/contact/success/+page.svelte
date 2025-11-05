9d71b3369b5dedc0278eea75eeb9349548fa2e55
robertbrice <rob.a.brice@gmail.com>
2025-11-05 08:29:11 +0000

    better margin in thank you contact

--- src/routes/contact/success/+page.svelte
+++ src/routes/contact/success/+page.svelte
@@ -6,6 +6,13 @@
 <main>
       <div class="container">
         <div class="fw-light title-size-reduced" style="padding-top: 2em;">Thank you.</div>	
-        <div class="fw-light title-size-reduced" style="padding-top: 1em; padding-bottom: 3em;">Your message has been sent.</div>
+        <div class="fw-light title-size-reduced space-below" style="padding-top: 1em; padding-bottom: 3em;">Your message has been sent.</div>
       </div>
 </main>
+
+<style>
+  .space-below {
+    margin-bottom: 400px;
+  }
+
+</style>
