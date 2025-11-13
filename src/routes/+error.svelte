97e931054392851f645c52dac849f2f38ffc73d9
robertbrice <rob.a.brice@gmail.com>
2025-11-13 17:29:49 +0000

    error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<svelte:head>
+    <title>Error | Robert Brice</title>
+    <meta name="description" content="404 error">
+</svelte:head>
+
+<main>
+      <div class="container">
+
+        <div class="fw-light display-2" style="padding-top: 1em;">404</div>	
+        <div class="fw-light title-size-reduced" style="padding-top: 1em;">This page could not be found.</div>	
+        <div class="fw-light title-size-reduced space-below" style="padding-top: 1em; padding-bottom: 3em;">Please check the address and try again or return to the <a href="http://www.robertbrice.net">home page</a>.</div>
+      </div>
+</main>
+
+<style>
+  .space-below {
+    margin-bottom: 400px;
+  }
+
+</style>
