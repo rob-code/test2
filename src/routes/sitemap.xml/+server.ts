c057155e5b23418ed987a71838975700d809458f
robertbrice <rob.a.brice@gmail.com>
2025-11-14 13:11:06 +0000

    updated canonical domain name

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,4 +1,4 @@
-const site = 'https://www.robertbrice.net'; // change this to reflect your domain
+const site = 'https://robertbrice.net'; // change this to reflect your domain
 const pages: string[] = ["", "shop", "collections", "about", "contact"]; // populate this with all the slugs you wish to include
 
 /** @type {import('./$types').RequestHandler} */
