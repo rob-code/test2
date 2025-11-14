68efd088c80354121b5607ee1f57fb95609d31c0
robertbrice <rob.a.brice@gmail.com>
2025-11-14 18:11:55 +0000

    clean up shop page

--- src/routes/shop/+page.svelte
+++ src/routes/shop/+page.svelte
@@ -1,15 +1,9 @@
 <script lang=ts> 
   import PageTitle from "../../components/pagetitle.svelte";
   import ShopRocket from "../../components/shoprocket_store.svelte";
-
-	// import type { PageProps } from './$types';
-	// let { data }: PageProps = $props();
-  // console.log(data);
-
 </script>
 
 <svelte:head>
-  <!-- all the other meta tags go in there for the page -->
   <title>Shop for Fine Art Prints | Robert Brice</title>
   <meta name="description" content="Shop for high quality fine art prints">
 </svelte:head>
